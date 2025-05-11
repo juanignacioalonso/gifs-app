34579c5807afa081fac9217ef8240ee82751a7be
juanignacioalonso <zalbak87@gmail.com>
2025-05-11 18:05:05 -0300

    RouterLinks - Desde componentes hijos 72

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,35 @@
 import {  Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,RouterLinkActive,
+  ],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+    menuOptions:MenuOption[] = [
+    {
+      icon:'fa-solid fa-chart-line',
+      label:'Trening',
+      subLabel: 'Gifs Populares',
+      route:'/dashboard/trending'
+    },
+    {
+      icon:'fa-solid fa-magnifying-glass',
+      label:'Buscador',
+      subLabel: 'Buscar gifs',
+      route:'/dashboard/search'
+    },
+
+  ];
+ }
