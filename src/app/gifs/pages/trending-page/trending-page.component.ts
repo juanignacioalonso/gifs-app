e472d038d59e17f7c20cfb011c0b4128f658e16a
juanignacioalonso <zalbak87@gmail.com>
2025-07-16 12:10:42 -0300

    Preservar posicion del scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,6 @@
-import {  Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { ScrollStateService } from './../../../shared/services/scroll-state.service';
+import {   AfterViewInit, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
+
 import { GifService } from '../../services/gifs.service';
 
 
@@ -7,13 +8,21 @@ import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-trending-page',
-  //imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit{
+
   gifService = inject(GifService)
+  ScrollStateService = inject(ScrollStateService)
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv')
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.ScrollStateService.trendingScrollState(); // esto es para que cuando se vuelva a renderizar el componente, se mantenga el scroll en la posicion que estaba antes
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -29,6 +38,7 @@ export default class TrendingPageComponent {
     //console.log({ scrollTotal: scrollTop + clientHeigth,scrollHeight})
 
     const isAtBottom = scrollTop + clientHeigth +300 >= scrollHeight;
+    this.ScrollStateService.trendingScrollState.set(scrollTop); // esto no es recomendado pq cualquier componente va a cambiar la señal y va a hacer que se vuelva a renderizar todo el componente
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
